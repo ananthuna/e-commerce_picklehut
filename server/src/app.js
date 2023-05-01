149765138178635f697f59426bbc81b3a14e2dc5
ananthu <ananthu4uuu@gmail.com>
2023-05-02 01:41:53 +0530

    deploye v0.01

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.resolve('./public')));
 app.use(fileUpload());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-const PORT = 3001
+const PORT = 3000
 
 app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
